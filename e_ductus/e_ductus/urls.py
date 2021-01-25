eada703ab1234f3d5e34d9b4c1514a1498d3af35
Marius-prog <msabaliauskas@gmail.com>
2021-01-25 18:31:01 +0100

    "some tested models"

--- e_ductus/e_ductus/urls.py
+++ e_ductus/e_ductus/urls.py
@@ -1,30 +1,24 @@
+"""e_ductus URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path
-from . import views
-from django.contrib.auth import views as auth_views
+from e_ductus.views import base, home
 
 urlpatterns = [
-    path('', views.BaseView.as_view(template_name='base.html')),
-
-    path('home/', views.HomeView.as_view(template_name='home.html')),
-
+    path('', base),
+    path('home/', home),
     path('admin/', admin.site.urls),
-
-    path('accounts/login/', auth_views.LoginView.as_view(),
-         name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(),
-         name='logout'),
-
-    path('mine/',
-         views.ManageCourseListView.as_view(),
-         name='manage_course_list'),
-    path('create/',
-         views.CourseCreateView.as_view(),
-         name='course_create'),
-    path('<pk>/edit/',
-         views.CourseUpdateView.as_view(),
-         name='course_edit'),
-    path('<pk>/delete/',
-         views.CourseDeleteView.as_view(),
-         name='course_delete'),
 ]
--- e_ductus/tests/test_e_ductus.py
+++ e_ductus/tests/test_e_ductus.py
@@ -1,52 +1,39 @@
-from unittest import TestCase
-
-# from django.test import TestCase
-from django.contrib.auth.models import User
-from django.http import request
-from django.test import Client, SimpleTestCase
-from django.views.generic import TemplateView
-
 from e_ductus.models import Subject, Course
 import pytest
-from django.urls import resolve, reverse
+from django.test import TestCase
 
-from e_ductus.views import HomeView
-
-from e_ductus.views import BaseView
-
-from e_ductus.views import OwnerMixin
+# from e_ductus.models import Course
 
 
-class HomeViewTest(TestCase):
-    def test_resolve_to_home_page_view(self):
-        resolver = resolve('/home/')
-        self.assertEqual(resolver.func.view_class, HomeView)
+def test_base(client):
+    assert client.get('/').status_code == 200
 
 
-class BaseViewTest(TestCase):
-    def test_resolve_to_base_page_view(self):
-        resolver = resolve('/')
-        self.assertEqual(resolver.func.view_class, BaseView)
+def test_home(client):
+    assert client.get('/home/').status_code == 200
 
 
 def test_admin(client):
     assert client.get('/').status_code == 200
 
 
+# class TestModels(TestCase):
+#     def test_models(self):
+#         title = Subject.objects.create(title="django_testing")
+#         slug = Subject.objects.create(slug="this my slug")
+#         self.assertEqual(str(title), "django_testing")
+#         self.assertEqual(str(slug), "this my slug")
+
 @pytest.mark.django_db
 def test_models():
     subject = Subject.objects.create(title="django1", slug="django")
+    # course = Course.objects.create(title="title")
+    # assert course.title == "title"
     assert subject.title == "django1"
     assert subject.slug == "django"
 
 
-class OwnerMixinTest(SimpleTestCase):
-    
-    class MyView(OwnerMixin, TemplateView):
-        pass
-    
-    def test_owner_mixin(self):
-        my_view = self.MyView()
-        context = my_view.get_context_data()
-        self.assertTrue(context)
-        
+# @pytest.mark.django_db
+# def test_models_course():
+#     course = Course.objects.create(title="title")
+#     assert course.title == "title"
