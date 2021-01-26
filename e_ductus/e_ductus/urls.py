ddf23aaae6734c859c9bc7934e136ce0f50486cb
Marius-prog <msabaliauskas@gmail.com>
2021-01-26 15:06:41 +0100

    "base views CBV"

--- e_ductus/e_ductus/urls.py
+++ e_ductus/e_ductus/urls.py
@@ -15,15 +15,28 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import base, home
+from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', base),
-    path('home/', home),
+    path('', views.BaseView.as_view()),
+    path('home/', views.HomeView.as_view()),
     path('admin/', admin.site.urls),
     path('accounts/login/', auth_views.LoginView.as_view(),
          name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(),
          name='logout'),
+
+    path('mine/',
+         views.ManageCourseListView.as_view(),
+         name='manage_course_list'),
+    path('create/',
+         views.CourseCreateView.as_view(),
+         name='course_create'),
+    path('<pk>/edit/',
+         views.CourseUpdateView.as_view(),
+         name='course_edit'),
+    path('<pk>/delete/',
+         views.CourseDeleteView.as_view(),
+         name='course_delete'),
 ]
