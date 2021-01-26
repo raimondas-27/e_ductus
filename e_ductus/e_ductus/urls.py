31fec4a3baecdad81b89766557f0a6d00961157c
Marius-prog <msabaliauskas@gmail.com>
2021-01-26 11:50:54 +0100

    "login, logout pages"

--- e_ductus/e_ductus/urls.py
+++ e_ductus/e_ductus/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from e_ductus.views import base, home
+from .views import base, home
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', base),
     path('home/', home),
     path('admin/', admin.site.urls),
+    path('accounts/login/', auth_views.LoginView.as_view(),
+         name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(),
+         name='logout'),
 ]
--- e_ductus/tests/test_e_ductus.py
+++ e_ductus/tests/test_e_ductus.py
@@ -1,8 +1,5 @@
 from e_ductus.models import Subject, Course
 import pytest
-from django.test import TestCase
-
-# from e_ductus.models import Course
 
 
 def test_base(client):
@@ -17,23 +14,8 @@ def test_admin(client):
     assert client.get('/').status_code == 200
 
 
-# class TestModels(TestCase):
-#     def test_models(self):
-#         title = Subject.objects.create(title="django_testing")
-#         slug = Subject.objects.create(slug="this my slug")
-#         self.assertEqual(str(title), "django_testing")
-#         self.assertEqual(str(slug), "this my slug")
-
 @pytest.mark.django_db
 def test_models():
     subject = Subject.objects.create(title="django1", slug="django")
-    # course = Course.objects.create(title="title")
-    # assert course.title == "title"
     assert subject.title == "django1"
     assert subject.slug == "django"
-
-
-# @pytest.mark.django_db
-# def test_models_course():
-#     course = Course.objects.create(title="title")
-#     assert course.title == "title"
