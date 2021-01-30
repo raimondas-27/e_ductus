fb1641297da68a5ae90376e81b8f0db74a2488f1
raimondas-27 <raimondasvilnius@yahoo.com>
2021-01-30 20:42:38 +0200

    some testing changes

--- e_ductus/tests/test_e_ductus.py
+++ e_ductus/tests/test_e_ductus.py
@@ -1,13 +1,44 @@
 from e_ductus.api.models import Subject, Course
 import pytest
+from django.urls import resolve
+from e_ductus.api.views import BaseView
+from e_ductus.api.views import OwnerMixin
+from unittest import TestCase
+import django.test
+from django.test import Client
+from django.contrib.auth.models import User
+from e_ductus.api.views import CourseDeleteView
 
 
-def test_base(client):
-    assert client.get('/').status_code == 200
+
+class CourseDeleteViewTest(TestCase):
+    def test_resolve_to_delete_page_view(self):
+        resolver = resolve("/courses/manage/course/delete.html/")
+        self.assertEqual(resolver.func.view_class, CourseDeleteView)
 
 
-def test_home(client):
-    assert client.get('/home/').status_code == 200
+class BaseViewTest(TestCase):
+    def test_resolve_to_home_page_view(self):
+        resolver = resolve("/home/")
+        self.assertEqual(resolver.func.view_class, BaseView)
+
+
+class OwnerMixingTest(TestCase):
+    def setUp(self):
+        self.user = User(username='jimish')
+        password = 'password'
+        self.user.set_password(password)
+        self.user.save()
+
+        self.client = Client()
+        self.client.login(username=self.user.username, password=password)
+
+
+
+    def test_resolve_getting_queryset(self):
+
+
+
 
 
 def test_admin(client):
