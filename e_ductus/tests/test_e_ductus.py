fca4d3b5869a4d6d77666677c9d362d9b2e1ed84
Marius Sabaliauskas <msabaliauskas@gmail.com>
2021-01-31 09:20:17 +0100

    add png

--- e_ductus/tests/test_e_ductus.py
+++ e_ductus/tests/test_e_ductus.py
@@ -2,19 +2,18 @@ from unittest import TestCase
 
 # from django.test import TestCase
 from django.contrib.auth.models import User
-
+from django.http import request
 from django.test import Client, SimpleTestCase
 from django.views.generic import TemplateView
+
 from e_ductus.models import Subject, Course
 import pytest
-from e_ductus.models import Subject
-from django.urls import resolve
+from django.urls import resolve, reverse
+
 from e_ductus.views import HomeView
+
 from e_ductus.views import BaseView
-from e_ductus.views import CourseDeleteView
-from e_ductus.views import CourseUpdateView
-from e_ductus.views import CourseCreateView
-from e_ductus.views import ManageCourseListView
+
 from e_ductus.views import OwnerMixin
 
 
@@ -27,47 +26,7 @@ class HomeViewTest(TestCase):
 class BaseViewTest(TestCase):
     def test_resolve_to_base_page_view(self):
         resolver = resolve('/')
-        self.assertEqual(resolver.func.view_class,BaseView)
-
-
-class CourseDeleteViewTest(TestCase):
-    def test_resolve_to_course_delete_page_view(self):
-        resolver = resolve('/<pk>/delete/')
-        self.assertEqual(resolver.func.view_class,CourseDeleteView)
-
-
-class CourseUpdateViewTest(TestCase):
-    def test_resolve_to_course_update_page_view(self):
-        resolver = resolve('/<pk>/edit/')
-        self.assertEqual(resolver.func.view_class,CourseUpdateView)
-
-
-class CourseCreateViewTest(TestCase):
-    def test_resolve_to_course_create_page_view(self):
-        resolver = resolve('/create/')
-        self.assertEqual(resolver.func.view_class,CourseCreateView)
-
-
-class ManageCourseListViewTest(TestCase):
-    def test_resolve_to_manage_course_list_page_view(self):
-        resolver = resolve('/mine/')
-        self.assertEqual(resolver.func.view_class,ManageCourseListView)
-
-
-class OwnerCourseEditMixinViewTest(TestCase):
-    def test_resolve_to_owner_course_edit_mixin_page_view(self):
-        resolver = resolve('/mine/')
-        self.assertEqual(resolver.func.view_class,ManageCourseListView)
-
-
-
-
-
-
-
-
-
-
+        self.assertEqual(resolver.func.view_class, BaseView)
 
 
 def test_admin(client):
