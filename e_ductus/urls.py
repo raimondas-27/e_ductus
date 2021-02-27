00554f35bc18f92d958e5cd3e0fcd22322581751
raimondas-27 <raimondasvilnius@yahoo.com>
2021-02-27 21:45:40 +0200

    final push

--- e_ductus/urls.py
+++ e_ductus/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
@@ -96,6 +98,6 @@ urlpatterns = [
     path('chat/', include('chat.urls', namespace='chat')),
 ]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL,
-#                           document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
